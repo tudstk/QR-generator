4fe5ca1b9bd27643ebd4195852ab71057368aafb
Tudor Stroescu <tudorstroescu@yahoo.com>
2022-05-18 00:06:33 +0300

    hide-qr-after-removing-input

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ generateBtn.addEventListener("click",()=>{
     let inputValue = qrInput.value;
     if(!inputValue) return;
     console.log(inputValue);
-    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
-    wrapper.classList.add("active");
+    generateBtn.innerText = "Generating QR code...";
+    qrImg.src=`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`;
+    qrImg.addEventListener("load", ()=>{
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR code"
+    })
+})
+
+qrInput.addEventListener("keyup", ()=>{
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
 })
